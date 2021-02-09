c29703ecec50275e128e8c1b7703c5e75217a789
Щипин Дмитрий <mrethorill@mail.ru>
2021-02-10 02:44:23 +0300

    Релиз

--- foodgram/foodgram/urls.py
+++ foodgram/foodgram/urls.py
@@ -3,14 +3,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.flatpages import views
+
 urlpatterns = [
     path('', include('recipes.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('accounts.urls')),
     path('about/', views.flatpage, {'url': '/about/'}, name='about'),
     path('author/', views.flatpage, {'url': '/author/'}, name='author'),
-    path('technologies/', views.flatpage, {'url': '/technologies/'}, name='technologies'),
-
+    path('technologies/', views.flatpage, {'url': '/technologies/'},
+         name='technologies'),
 
 ]
 
